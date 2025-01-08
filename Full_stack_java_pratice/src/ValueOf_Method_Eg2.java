eb67c5f1ef81de57b1e6ad659fb226658a8a2e6d
pavan-png <mohanpavankalyan52@gmail.com>
2025-01-08 10:23:36 +0530

    commit = 110

--- Full_stack_java_pratice/src/ValueOf_Method_Eg2.java
+++ Full_stack_java_pratice/src/ValueOf_Method_Eg2.java
@@ -2,7 +2,16 @@ public class ValueOf_Method_Eg2 {
     public static void main(String[] args) {
         Integer i1 = Integer.valueOf("1111");
         System.out.println(i1);
-        Integer i2 = Integer.valueOf("1111",4);
+        Integer i2 = Integer.valueOf("11111",4);
+        /*
+        1
+        1*4 every time multiplied by 4 since radix is 4.
+        4*4
+        16*4
+        64*4
+        total 1+4+16+64+256 = 341
+         */
+
         System.out.println(i2);
         Integer i4 = Integer.valueOf("11",36);
         System.out.println(i4);

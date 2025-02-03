68f36f5fd02ae988a7409b6fb73b3835c9c545cb
pavan-png <mohanpavankalyan52@gmail.com>
2025-02-03 10:49:11 +0530

    commit = 118

--- Full_stack_java_pratice/src/Functional_Interface_With_Optional_Parameters_DataType.java
+++ Full_stack_java_pratice/src/Functional_Interface_With_Optional_Parameters_DataType.java
@@ -6,7 +6,9 @@ interface  Add{
 public class Functional_Interface_With_Optional_Parameters_DataType {
     public static void main(String[] args) {
 
-        Add obj = (a,b)->{
+        Add obj = (  a,  b)->{
+            // data types should be given to all the parameters (or) should not be declared for all the parameters
+            // we cannot specify data types only for some parameters, it leads to compile time error
             int c = a+b;
             System.out.println(c);
 

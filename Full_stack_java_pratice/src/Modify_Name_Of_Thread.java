3fcdb9b98f4727f6f3c14b6c51edcb40ed59ba95
pavan-png <mohanpavankalyan52@gmail.com>
2025-02-10 22:01:28 +0530

    commit = 121

--- Full_stack_java_pratice/src/Modify_Name_Of_Thread.java
+++ Full_stack_java_pratice/src/Modify_Name_Of_Thread.java
@@ -6,7 +6,7 @@ public class Modify_Name_Of_Thread extends Thread {
         System.out.println(t.getName());
         Thread.currentThread().setName("pavan-one-man-army");
         System.out.println(Thread.currentThread().getName());
-        System.out.println(10/0);
+        System.out.println(10/5);
     }
 
 

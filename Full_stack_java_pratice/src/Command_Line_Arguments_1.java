30d7c223e3f9ef6f0167974b35ac2e7346799ab0
pavan-png <mohanpavankalyan52@gmail.com>
2025-11-28 07:39:05 +0530

    commit = 124

--- Full_stack_java_pratice/src/Command_Line_Arguments_1.java
+++ Full_stack_java_pratice/src/Command_Line_Arguments_1.java
@@ -1,7 +1,12 @@
 public class Command_Line_Arguments_1 {
-    public static void main(String []args) {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Please provide at least 2 arguments!");
+            return; // exits the entire method
+        }
+
         System.out.println(args[0]);
         System.out.println(args[1]);
-        // if the arguments are not provided and above code is executed it will lead to ArrayIndexOutOfBounds Exception
     }
 }
--- Full_stack_java_pratice/src/Main_Method_Overloading.java
+++ Full_stack_java_pratice/src/Main_Method_Overloading.java
@@ -1,54 +1,48 @@
-/*  we can overload the main() method. But remember that the JVM always calls the main()
-method with regular signature i.e is public static void main(String[] args) .
-other main methods are ignored by jvm.
-
- */
+import java.util.Arrays;
 
 public class Main_Method_Overloading {
-    public static void main(byte[] args)
-    {
-        System.out.println("this byte main method");
+
+    public static void main(byte[] args) {
+        System.out.println("this is byte main method");
+        System.out.println(Arrays.toString(args));
     }
 
     public static void main(short[] args) {
-
         System.out.println("this is short main method");
+        System.out.println(Arrays.toString(args));
     }
 
     public static void main(int[] args) {
-
-        System.out.println("this is int  main method");
+        System.out.println("this is int main method");
+        System.out.println(Arrays.toString(args));
     }
 
     public static void main(long[] args) {
         System.out.println("this is long main method");
-        System.out.println("the number passed as long argument is " +args);
+        System.out.println(Arrays.toString(args));
     }
-    public static void main(float[] args) {
 
+    public static void main(float[] args) {
         System.out.println("this is float main method");
+        System.out.println(Arrays.toString(args));
     }
 
     public static void main(double[] args) {
-
         System.out.println("this is double main method");
+        System.out.println(Arrays.toString(args));
     }
 
-
     public static void main(char[] args) {
-
         System.out.println("this is char main method");
+        System.out.println(Arrays.toString(args));
     }
 
     public static void main(boolean[] args) {
         System.out.println("this is boolean main method");
-        System.out.println("boolean argument is " +args);
+        System.out.println(Arrays.toString(args));
     }
 
-
     public static void main(String[] args) {
-        // main method with regular signature is automatically called by the jvm.
-        // it does not need to be invoked manually.
         System.out.println("this is original main method");
     }
 }

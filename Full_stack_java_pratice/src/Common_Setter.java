9b9774fb14446e7969ad969e865aca116206b786
pavan-png <mohanpavankalyan52@gmail.com>
2026-01-27 18:05:04 +0530

    commit = 133

--- Full_stack_java_pratice/src/Common_Setter.java
+++ Full_stack_java_pratice/src/Common_Setter.java
@@ -1,16 +1,16 @@
-class Studen2{
-    private String namee;
-    private int  age ;
+class Student2 {
+    private String name;
+    private int age;
     private String city;
 
-    public void setData(String namee ,int age , String city){
-        this.namee = namee;
+    public void setData(String name, int age, String city){
+        this.name = name;
         this.age = age;
         this.city = city;
     }
 
-    public String getNamee() {
-        return namee;
+    public String getName() {
+        return name;
     }
 
     public int getAge() {
@@ -21,14 +21,14 @@ class Studen2{
         return city;
     }
 }
+
 public class Common_Setter {
     public static void main(String[] args) {
-        Studen2 obj = new Studen2();
-        obj.setData("pavan", 24,"vijayawada");
-        System.out.println(obj.getNamee());
+        Student2 obj = new Student2();
+        obj.setData("pavan", 24, "vijayawada");
+
+        System.out.println(obj.getName());
         System.out.println(obj.getAge());
         System.out.println(obj.getCity());
     }
-
-
 }
--- Full_stack_java_pratice/src/Encapsulation.java
+++ Full_stack_java_pratice/src/Encapsulation.java
@@ -1,46 +1,46 @@
-class Student1{
-    private String name ;
+class Student1 {
+    private String name;
     private int age;
-    private String city ;
+    private String city;
 
-    /* if you want you can write some extra logic to getter and setter , to restrict the user to give only specific input
-      */
-    void setName(String name){
-
+    public void setName(String name) {
         this.name = name;
     }
-    String getName(){
 
+    public String getName() {
         return name;
     }
 
-    void setAge(int age){
-
-        this.age = age;
+    public void setAge(int age) {
+        if (age > 0) {          // controlled access
+            this.age = age;
+        }
     }
-    int getAge() {
 
+    public int getAge() {
         return age;
     }
-    void setCity(String city){
 
+    public void setCity(String city) {
         this.city = city;
     }
-    String getCity(){
 
+    public String getCity() {
         return city;
     }
 }
+
 public class Encapsulation {
     public static void main(String[] args) {
         Student1 obj = new Student1();
-        obj.setName("pavan");
-        String name = obj.getName();
-        System.out.println(name);
+
+        obj.setName("Pavan");
+        System.out.println(obj.getName());
+
         obj.setAge(25);
         System.out.println(obj.getAge());
-        obj.setCity("vijayawada");
-        System.out.println(obj.getCity());
 
+        obj.setCity("Vijayawada");
+        System.out.println(obj.getCity());
     }
 }
